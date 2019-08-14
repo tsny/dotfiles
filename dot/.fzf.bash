266ba6bb0d4a25fb4c08242de9a339b20b59560b
tsny <tsny700@gmail.com>
2019-08-13 22:40:07 -0500

    more understanding of shell variables

--- dot/.fzf.bash
+++ dot/.fzf.bash
@@ -14,8 +14,10 @@ fi
 source "/usr/local/opt/fzf/shell/key-bindings.bash"
 
 alias ef='vim $(fzf --height 40%)'
+alias ffd='__fzf_cd__'
 bind -x '"\C-f":"`__fzf_cd__`"'
 
 export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,Music,Pictures} --type f"
+export FZF_ALT_C_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build,Music,Pictures} --type d"
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 
