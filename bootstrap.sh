1401856804ef3d2e16e4a1dec5b23491791bbdfb
tsny <tsny700@gmail.com>
2024-05-15 18:57:26 -0500

    fix line 34

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,7 +31,7 @@ if [ ! -d "~/.vim/autoload" ]; then
 fi
 
 # Install oh-my-zsh
-if [[ "$ZSH" != *"oh"* ]]; then
+if [[ ! "$ZSH" == *"oh"* ]]; then
     read -p "Do you want to install Oh My Zsh? (y/n): " choice
     if [[ $choice == "y" || $choice == "Y" ]]; then
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
