f9d23547256a77c3ad5b84fcfca25602a53a469a
tsny-houzz <taylor.snyder@houzz.com>
2025-07-07 13:27:29 -0500

    better

--- dot/.config/tmux-powerline/themes/my-theme.sh
+++ dot/.config/tmux-powerline/themes/my-theme.sh
@@ -102,12 +102,12 @@ fi
 # shellcheck disable=SC1143,SC2128
 if [ -z "$TMUX_POWERLINE_LEFT_STATUS_SEGMENTS" ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234"
+		#"tmux_session_info 148 234"
 		"hostname 33 0"
-		"mode_indicator 165 0"
+		#"mode_indicator 165 0"
 		#"ifstat 30 255"
 		#"ifstat_sys 30 255"
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
+		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
 		#"vpn 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}"
 		#"wan_ip 24 255"
 		# "vcs_branch 29 88"
@@ -134,7 +134,7 @@ if [ -z "$TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS" ]; then
 		#"weather 37 255"
 		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}"
 		#"xkb_layout 125 117"
-		"date_day 235 136"
+		#"date_day 235 136"
 		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
 		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}"
